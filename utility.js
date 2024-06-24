bc7a2f1717a93db549380611bae129798e79a0ca
Davis Maloch (Fl. Memory) <133890108+admaloch-dos@users.noreply.github.com>
2024-06-24 09:18:56 -0400

    Delete utility.js

--- utility.js
+++
@@ -1,8 +0,0 @@
-// trigger change with js
-const triggerEventFunc = (input, value) => {
-    const e = new Event("change");
-    const element = document.querySelector(input)
-    element.value = value;
-    element.dispatchEvent(e);
-}
-
